7f84dfd49d547dd7a1834ba3ba4028aad4370821
Samantha Gatt <smfgatt@gmail.com>
2019-02-24 15:14:53 -0500

    'created JSON Poll from template https://github.com/twostraws/vapor-clean'

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "VaporApp",
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.1.0")),
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"]),
+    ]
+)
